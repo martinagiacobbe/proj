7c047c7240ebae6da07e0a7f128c51463ea21c72
martinagiacobbe <39266295+martinagiacobbe@users.noreply.github.com>
2018-07-06 12:38:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,65 @@
 from flask import Flask, render_template, request, json, url_for, redirect, send_from_directory
 import os
-#from keras.models import load_model
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import shutil
+from keras.models import load_model
+import pylab as pl
+import numpy as np
+from PIL import Image
+from keras.preprocessing.image import img_to_array, load_img
+import cv2
+import keras
+import mpld3
+from keras import backend as K
 app = Flask(__name__)
 #app = Flask(__name__, static_url_path='/static')
-UPLOAD_FOLDER = os.path.basename('uploads')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    pics = os.listdir('static/gallery/')
+    return render_template('index.html', pics=pics,init=False,thepred=False)
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    file = request.files['image']
-    #file.filename
-    filename='photo.jpg'
-    f = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    
-    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
-    file.save(f)
-    
-    return redirect(url_for('uploaded_file', filename=filename))
+@app.route('/show/<pic>')
+def uploaded_file(pic):
+    pics = os.listdir('static/gallery/')
+    #shutil.copy('static/gallery/'+pic,TEST_FOLDER)
+    return render_template('index.html',pics=pics, pic=pic, init=True,thepred=False)
 
-@app.route('/show/<filename>')
-def uploaded_file(filename):
-    return render_template('index.html', filename=filename, init=True)
+#@app.route('/test/<pic>', methods=['GET'])
+#def send_file(pic):
+#    return send_from_directory('static/gallery/', pic, cache_timeout=1)
 
-@app.route('/uploads/<filename>')
-def send_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename, cache_timeout = 1)
+@app.route('/show/<pic>/button')
+def button(pic):
+    K.clear_session()
+    pics = os.listdir('static/gallery/')
+    path='static/gallery/'+pic
+    img = load_img(path)
+    x = img_to_array(img)*1.0/255
+    xresized = cv2.resize(x, (128,128))
+    inputarray = np.expand_dims(xresized, axis=0)
+    loaded_model = keras.models.Sequential()
+    loaded_model = load_model('nuovarete1.h5')
+    cls = loaded_model.predict_classes(inputarray)
+    prob=loaded_model.predict_proba(inputarray)
+    explode = (0.05,0.05)
+    labels = ['healty', 'ill']
+    sizes = [1.0-prob[0,0],prob[0,0]]
+    colors = ['#ff9999','#66b3ff']
 
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, colors = colors, labels=labels, autopct='%1.1f%%', startangle=90)
+    centre_circle = plt.Circle((0,0),0.70,fc='white')
+    fig = plt.gcf()
+    fig.gca().add_artist(centre_circle)
+    ax1.axis('equal')
+    plt.tight_layout()
+    #plt.show()
+    fig1.savefig('static/results/'+pic)
+    mpld3.fig_to_html(fig)
+    return render_template('index.html', pics=pics, pic=pic, init=False, thepred=True, prob=prob,cls=cls, url ='/static/results/'+pic)
 
-@app.route('/retry', methods=["GET", "POST"])
-def retry():
-    return render_template('index.html', init=False)
-
-
-#def load_model():
-#    loaded_model = load_model('nuovarete1.h5')
-#    probability = loaded_model.predict()
 
 if __name__=="__main__":
     app.run()
